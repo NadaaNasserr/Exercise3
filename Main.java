c12bd4526dafccb8b1af0e60203519ac47e90e1f
NadaaNasserr <148685934+NadaaNasserr@users.noreply.github.com>
2023-10-25 23:00:27 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,124 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+
+
+        //Exercise1
+//        for (int i = 1; i <=100 ; i++) {
+//
+//            if(i % 3 == 0 && i % 5 == 0 ){
+//                System.out.println("FizzBuzz");
+//            }
+//            else if(i % 3 == 0 ){
+//                System.out.println("Fizz");
+//            }
+//            else if(i % 5 == 0 ){
+//                System.out.println("Buzz");
+//            }
+//            else System.out.println(i);
+//        }
+
+
+//        //Exercise2
+//        System.out.println(" Input a string:  ");
+//        String input =S.nextLine();
+//        String reverse = "";
+//        for (int i =input.length()-1 ; i>= 0; i--){
+//            reverse+= input.charAt(i);
+//        }
+//        System.out.printf("Reverse string: " + reverse);
+
+
+//Exercise3
+//        System.out.println("Enter the number  ");
+//        int num =S.nextInt();
+//        int factorialvalue = 1;
+//        for (int i = 1; i<= num ; i++ ){
+//            factorialvalue *=i;
+//        }
+//        System.out.println("factorial value of number " + num
+//        + "! = " + factorialvalue);
+
+
+        //Exercise4
+//        System.out.println("Enter the number 1  ");
+//        int num1 =S.nextInt();
+//        System.out.println("Enter the number 2  ");
+//        int num2 =S.nextInt();
+//            int x=1;
+//        for (int i = 1 ; i<=num2 ; i++){
+//            x *= num1; }
+//            System.out.println(num1 + " power of " + num2 + "= " +x);
+
+
+
+//        //Exercise5
+//        int sumEvenNum = 0;
+//        int sumOddNum = 0;
+//        for (int i = 0; i <= 10; i++) {
+//            if (i % 2 == 0) {
+//                sumEvenNum = sumEvenNum + i;
+//            } else
+//                sumOddNum +=i;
+//        }
+//        System.out.println("Sum Even Number = " + sumEvenNum);
+//        System.out.println("Sum Odd Number = " + sumOddNum);
+
+
+
+
+////Exercise6
+//            int num = 0;
+//
+//        do {
+//            System.out.println("please enter number");
+//            num =S.nextInt();
+//            if(num % 2 == 1){
+//                System.out.println("the number is a prime number ");
+//                break;
+//            }
+//            else System.out.println("the number is not prime");
+//
+//        }while (num > 0);
+
+
+
+        //Exercise7
+//        for (int i = 1; i <= 4; i++){
+//            System.out.println("Week " + i);
+//            for (int j = 1; j <= 7; j++) {
+//                System.out.println("Day" + j);
+//            }
+//        }
+
+
+
+       // Exercise8
+//        System.out.println(" Input a string:  ");
+//          String input =S.nextLine();
+//           String test = "";
+//
+//            for  (int i = input.length()-1 ; i >= 0 ; i--) {
+//                test = test + input.charAt(i);
+//            }if(input.equals(test))
+//                    System.out.println("String is palindrome");
+//
+//             else System.out.println("String is not palindrome");
+//
+
+
+
+
+
+
+
+          }
+
+    }
+
+
+
+
+
